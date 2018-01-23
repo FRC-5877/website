b31a64174dd6792ce493d92407afd4ec0f83f563
Koo <kootg04@gmail.com>
2018-01-23 14:36:17 -0800

    2018 design

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,19 @@
 var express = require('express');
 var router = express.Router();
 var email = require('emailjs');
+var now = new Date();
+var season = new Date(2018, 6);
+var year = now.getFullYear();
 var server = email.server.connect({
   hode: 'localhost'
 });
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index');
+  if(now < season)
+    res.render('index-' + year, {year: year});
+  else
+    res.render('index', {year: year});
 });
 
 module.exports = router;
