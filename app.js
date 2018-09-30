65e42e73b18dcb56e88f5a71d1aa1bd1525edf7f
Forsyth, Steve [CS] <Steve.Forsyth@contractor.cengage.com>
2018-09-30 15:29:52 -0700

    a few fixups here and there

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
-var forceSSL = require('express-force-ssl');
+//var forceSSL = require('express-force-ssl');
 var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -20,7 +20,7 @@ app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(forceSSL);
+//app.use(forceSSL);
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
