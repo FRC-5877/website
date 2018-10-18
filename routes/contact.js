007a56c62f9f30b205b0d0ccde1ece083bd9a61a
Douglas DeVicariis <kootg04@gmail.com>
2018-10-17 22:42:07 -0700

    Minro changes to contact reply

--- routes/contact.js
+++ routes/contact.js
@@ -10,8 +10,8 @@ router.post('/', function(req, res, next) {
   console.log(req.body);
   if(req.body && req.body.fname && req.body.email && req.body.subj) {
     sendmail({
-      html:    "New Contact Request from Website\nName: " + req.body.fname + "\nEmail: " + req.body.email + "\nSubject: " + req.body.subj + "\nMessage: " + req.body.mssg, 
-      from:    "contact@team5877.com",
+      html:    "New Contact Request from Website<br>Name: " + req.body.fname + "<br>Email: " + req.body.email + "<br>Subject: " + req.body.subj + "<br>Message: " + req.body.mssg, 
+      from:    "Website <contact@team5877.com>",
       to:      "doug@team5877.com, steve@team5877.com",
       subject: "Contact from Website"
     }, function(err, reply) {
