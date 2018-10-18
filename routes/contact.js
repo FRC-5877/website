8a4029ad140883791051efc136bf6ba85d9b51e1
Douglas DeVicariis <kootg04@gmail.com>
2018-10-17 22:30:29 -0700

    Fixes everything

--- routes/contact.js
+++ routes/contact.js
@@ -1,9 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var email = require('emailjs');
-var server = email.server.connect({
-  hode: 'localhost'
-});
+const sendmail = require('sendmail')();
 
 router.get('/', (req, res, next) => {
    res.status(200);
@@ -12,15 +9,16 @@ router.get('/', (req, res, next) => {
 router.post('/', function(req, res, next) {
   console.log(req.body);
   if(req.body && req.body.fname && req.body.email && req.body.subj) {
-    server.send({
-      text:    "New Contact Request from Website\nName: " + req.body.fname + "\nEmail: " + req.body.email + "\nSubject: " + req.body.subj + "\nMessage: " + req.body.mssg, 
-      from:    "Do Not Reply <donotreply@team5877.com>", 
+    sendmail({
+      html:    "New Contact Request from Website\nName: " + req.body.fname + "\nEmail: " + req.body.email + "\nSubject: " + req.body.subj + "\nMessage: " + req.body.mssg, 
+      from:    "contact@team5877.com",
       to:      "contact@team5877.com",
       subject: "Contact from Website"
-    }, function(err, message) {
-      console.log(err || message);
+    }, function(err, reply) {
       if(!err)
         res.send({code: 1});
+      else
+        console.log(`ERROR! ${err}`);
     });
   } else {
     res.send({code: 0});
